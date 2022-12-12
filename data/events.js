17eeac21390f0c2d28a6fd93df569f04503debff
A-very-Cunning-ham <ave210knife@gmail.com>
2022-12-12 02:27:35 -0500

    Add additional event data functions

--- data/events.js
+++ data/events.js
@@ -1,8 +1,11 @@
-const helpers = require('../../helpers');
-const users = require('./users');
-const mongoCollections = require('../config/mongoCollections');
+const helpers = require("../../helpers");
+const users = require("./users");
+const mongoCollections = require("../config/mongoCollections");
 const user_collection = mongoCollections.user_collection;
 const event_collection = mongoCollections.event_collection;
+const { ObjectId } = require("mongodb");
+
+const maxImageSizeMB = 5;
 
 const createEvent = async (
 	eventName,
@@ -12,13 +15,14 @@ const createEvent = async (
 	postedBy,
 	tags,
 	description,
-	capacity
+	capacity,
+	imageData
 ) => {
 	//
 	const event_collection_c = await event_collection();
 
-	helpers.errorIfNotProperString(eventName, 'eventName');
-	helpers.errorIfNotProperString(location, 'location');
+	helpers.errorIfNotProperString(eventName, "eventName");
+	helpers.errorIfNotProperString(location, "location");
 	helpers.errorIfNotProperDateTime(startTime);
 	helpers.errorIfNotProperDateTime(endTime);
 	if (Date.parse(startTime) >= Date.parse(endTime)) {
@@ -26,16 +30,16 @@ const createEvent = async (
 	}
 
 	//check if user exists
-	helpers.errorIfNotProperUserName(postedBy, 'postedBy');
+	helpers.errorIfNotProperUserName(postedBy, "postedBy");
 	postedBy = postedBy.trim();
 	let user = await users.getUserData(postedBy);
 	if (!user) throw `No user present with userName: ${postedBy}`;
 
 	if (tags) {
-		helpers.errorIfNotProperString(tags, 'Tags');
-		tags = tags.split(',');
+		helpers.errorIfNotProperString(tags, "Tags");
+		tags = tags.split(",");
 	}
-	helpers.errorIfNotProperString(description, 'description');
+	helpers.errorIfNotProperString(description, "description");
 
 	//	helpers.errorIfNotProperString(college, 'college');
 	college = user.college;
@@ -45,6 +49,14 @@ const createEvent = async (
 
 	if (capacity < 1 || capacity % 1 > 0) {
 		throw `Invalid Capacity provided`;
+	}
+
+	if (imageData.size > 1024 * maxImageSizeMB) {
+		throw `Image size must be below ${maxImageSizeMB} MB`;
+	}
+
+	if (!imageData.mimetype.contains("image")) {
+		throw `File must be an image`;
 	}
 
 	let new_event = {
@@ -60,7 +72,7 @@ const createEvent = async (
 		usersRegistered: [],
 		numFavorite: 0,
 		favoriteUsers: [],
-		images: [],
+		image: image.data,
 		college: college,
 		comments: [],
 	};
@@ -72,4 +84,39 @@ const createEvent = async (
 	} else {
 		return { eventInserted: true };
 	}
+};
+
+const getEventById = async (id) => {
+	if (!id) throw "You must provide an ID to search for";
+	if (typeof id !== "string") throw "ID must be a string";
+	if (id.trim().length === 0)
+		throw "ID cannot be an empty string or just spaces";
+	id = id.trim();
+	if (!ObjectId.isValid(id)) throw "invalid object ID";
+
+	const event_collection_c = await event_collection();
+	const event = await event_collection_c.findOne({ _id: ObjectId(id) });
+	if (!event) throw "No event with that id";
+
+	event._id = event._id.toString();
+
+	return event;
+};
+
+const getUpcomingEvents = async (college) => {
+	helpers.errorIfNotProperString(college, "College");
+
+	college = college.trim();
+
+	const event_collection_c = await event_collection();
+	const events = await event_collection_c.find({
+		college: college,
+		startTime: { $gte: new Date() },
+	});
+
+	if (!events) throw "No events found";
+
+	//   TODO: check if _id needs to be converted to string
+
+	return events;
 };
---
+++ validation.js
@@ -0,0 +1,4 @@
+// validation functions to be used for the client, routes, and data functions 
+const helpers = require('../../helpers');
+
+
