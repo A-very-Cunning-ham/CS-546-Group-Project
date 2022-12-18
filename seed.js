42e28c10ab822b175339edfed7c848cd2e75933a
A-very-Cunning-ham <ave210knife@gmail.com>
2022-12-18 02:56:17 -0500

    Add images and update seed file

--- seed.js
+++ seed.js
@@ -1,9 +1,16 @@
 const data = require('./data');
 const mongoConnection = require('./config/mongoConnection');
 const commentData = data.comments;
-const eventData = data.events;
+
 const userData = data.users;
+const eventData = data.events;
 const { ObjectId } = require('mongodb');
+const testImages = require("./testImages");
+
+const catImage = testImages[0].image;
+const pokemonImage1 = testImages[1].image
+const pokemonImage2 = testImages[2].image
+
 
 async function main() {
     const db = await mongoConnection.dbConnection();
@@ -11,9 +18,11 @@ async function main() {
     let larry = await userData.createUser("larrystooge", "Secret_Password1", "Larry", "Fine", "Stevens Institute of Technology");
     let curly = await userData.createUser("curlystooge", "Secret_Password2", "Curly", "Howard", "Stevens Institute of Technology");
     let moe = await userData.createUser("moestooge", "Secret_Password3", "Moe", "Howard", "Rutgers University");
-    //TODO = seed images
-    let levent = await eventData.createEvent("Larry's Event", "Moe's House", '2022-12-30T12:00', '2022-12-31T12:00', "larrystooge", "fun, awesome, event", "Nyucknyucknyucknyucknyuck!", 20, )
+
+    let levent = await eventData.createEvent("Larry's Event", "Moe's House", '2022-12-30T12:00', '2022-12-31T12:00', "larrystooge", "fun, awesome, event", "Nyucknyucknyucknyucknyuck!", "20", catImage)
     console.log("Finished seeding");
     await mongoConnection.closeConnection();
     return;
 }
+
+main().catch(console.log);
