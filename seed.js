d3a9df2de8623197b899e6e5f0d638593641c8e4
Angelo Nicosia <anicosi1@stevens.edu>
2022-12-18 20:26:09 -0500

    more work on seed file

--- seed.js
+++ seed.js
@@ -23,8 +23,27 @@ async function main() {
     //create events
     let levent = await eventData.createEvent("Larry's Event", "Larry's House", '2022-12-30T12:00', '2022-12-31T12:00', "larrystooge", "fun, awesome, event", "Nyucknyucknyucknyucknyuck!", "20", catImage);
     let cevent = await eventData.createEvent("Curly's Event", "Curly's House", '2022-12-25T18:00', '2022-12-25T21:30', "curlystooge", "christmas, party, presents", "Merry Christmas to all!", "15", pokemonImage1);
-    let mevent = await eventData.createEvent("Curly's Event", "Curly's House", '2022-12-31T23:00', '2022-01-01T01:45', "moestooge", "hello, hi, howdy, aloha", "Hey, where is everybody at Rutgers?", "5", pokemonImage2);
+    let mevent = await eventData.createEvent("Curly's Event", "Curly's House", '2022-12-31T23:00', '2022-01-01T01:45', "moestooge", "hello, hi, howdy, aloha", "Party for Rutgers peeps ONLY!", "5", pokemonImage2);
     //now test register, favorite, unregister, unfavorite, cancel, comment
+
+    let lregister = await eventData.registerForEvent(larry.username, levent._id);
+    let lregister2 = await eventData.registerForEvent(larry.username, cevent._id);
+    let cregister = await eventData.registerForEvent(curly.username, cevent._id);
+    let mregister = await eventData.registerForEvent(moe.username, mevent._id);
+
+    let cfavorite = await eventData.favoriteEvent(curly.username, cevent._id);
+    let mfavorite = await eventData.favoriteEvent(moe.username, mevent._id);
+
+    let lcomment = await commentData.createComment(cevent._id, larry.username, "Can't wait!");
+    let mcomment = await commentData.createComment(mevent._id, moe.username, "Hey, where IS everybody?");
+
+    let lunregister = await eventData.unregisterForEvent(larry.username, cevent._id);
+    let lcomment2 = await commentData.createComment(cevent._id, larry.username, "Sorry, something came up, I can't go");
+    let ccomment = await commentData.createComment(cevent._id, curly.username, "It just won't be a party without Larry!");
+
+    let munfavorite = await eventData.unfavoriteEvent(moe.username, mevent._id);
+
+
     console.log("Finished seeding");
     await mongoConnection.closeConnection();
     return;
